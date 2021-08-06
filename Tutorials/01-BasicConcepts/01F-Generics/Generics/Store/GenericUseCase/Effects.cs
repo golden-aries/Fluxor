49f7c5376c4a06506959868b7525dab7a1cab151
Peter Morris <mrpmorris@gmail.com>
2021-08-06 12:19:22 +0100

    Update effect text

--- Tutorials/01-BasicConcepts/01F-Generics/Generics/Store/GenericUseCase/Effects.cs
+++ Tutorials/01-BasicConcepts/01F-Generics/Generics/Store/GenericUseCase/Effects.cs
@@ -3,16 +3,16 @@ using System.Threading.Tasks;
 
 namespace BasicConcepts.Generics.Store.GenericUseCase
 {
-	public static class Effects<TInnerState>
+	public static class Effects<T>
 	{
 		[EffectMethod]
 		public static async Task HandleStartBusyOperationActionAsync(
-			StartBusyOperationAction<TInnerState> action,
+			StartBusyOperationAction<T> action,
 			IDispatcher dispatcher)
 		{
-			System.Console.WriteLine($"\tEffect decrementing {typeof(TInnerState).Name}.IsBusyCount in 5 seconds");
+			System.Console.WriteLine($"\tEffect decrementing {typeof(T).Name}.IsBusyCount in 5 seconds");
 			await Task.Delay(5000);
-			dispatcher.Dispatch(new EndBusyStateAction<TInnerState>());
+			dispatcher.Dispatch(new EndBusyStateAction<T>());
 		}
 	}
 }
